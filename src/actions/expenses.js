a27c50da8d4410f492c2bfe42c1f3a4d4840e7bf
Andrew <arhorn@smcm.edu>
2018-02-06 22:38:49 +0700

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,14 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -61,8 +69,3 @@ export const startSetExpenses = () => {
     });
   };
 };
-
-
-// 1. Fetch all expense data once
-// 2. Parse that data into an array
-// 3. Dispatch SET_EXPENSES
