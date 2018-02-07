51c8c0c51a1cff8264bd0c76e2867dd98abda797
Andrew <arhorn@smcm.edu>
2018-02-07 15:06:17 +0700

    Add private routes

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
-import Header from '../../components/Header';
-
-// react-test-renderer
+import { Header } from '../../components/Header';
 
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
-  expect(toJSON(wrapper)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
+  const wrapper = shallow(<Header startLogout={() => {}} />);
+});
+
+test('should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout}/>);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
 });
